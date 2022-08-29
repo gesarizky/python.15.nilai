613892a08af8cd8a590079f33bd9dcbbba8f586a
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-08-30 06:53:47 +0700

    Add files via upload

---
+++ kode.15.nilai.py
@@ -0,0 +1,18 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+#Menginput Angka
+angka = float(input("Tulis Sebuah Angka: "))
+
+#Menampilkan Kondisi Angka Positif
+if angka > 0:
+   print("Angka Positif")
+
+#Menampilkan Kondisi Angka Nol   
+elif angka == 0:
+   print("Angka Nol")
+
+#Menampilkan Kondisi Angka Negatif
+else:
+   print("Angka Negatif")
